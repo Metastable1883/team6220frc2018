f3180de6daf95d9455f55643123971de84c34b6c
Drew Moran <drewrmoran@gmail.com>
2018-02-16 19:24:14 -0500

    Initial commit

--- src/main/java/frc/team6220/robot/Robot.java
+++ src/main/java/frc/team6220/robot/Robot.java
@@ -22,27 +22,58 @@ import edu.wpi.first.wpilibj.DriverStation;
  */
 public class Robot extends IterativeRobot {
     private DifferentialDrive m_myRobot;
-    private WPI_TalonSRX fauh = new WPI_TalonSRX(4);
-    private WPI_TalonSRX tree = new WPI_TalonSRX(3);
-    private WPI_TalonSRX too = new WPI_TalonSRX(2);
-    private WPI_TalonSRX juan = new WPI_TalonSRX(1);
-    private SpeedControllerGroup gogo = new SpeedControllerGroup(juan,too);
-    private SpeedControllerGroup gogo2 = new SpeedControllerGroup(tree,fauh);
-    private XboxController xbax;
-    private double dootdoot = 2;
+    private DigitalInput lsOut = new DigitalInput(1);
+    private DigitalInput lsIn = new DigitalInput(2);
+    private DigitalInput lsUp = new DigitalInput(3);
+    private DigitalInput lsDown = new DigitalInput(4);
+    private Counter countOut = new Counter(lsOut);
+    private Counter countIn = new Counter(lsIn);
+    private Counter countUp = new Counter(lsUp);
+    private Counter countDown = new Counter(lsDown);
+    private WPI_TalonSRX grip = new  WPI_TalonSRX(6);
+    private WPI_TalonSRX height  = new WPI_TalonSRX(5);
+    private WPI_TalonSRX four = new WPI_TalonSRX(4);
+    private WPI_TalonSRX three = new WPI_TalonSRX(3);
+    private WPI_TalonSRX two = new WPI_TalonSRX(2);
+    private WPI_TalonSRX one = new WPI_TalonSRX(1);
+    private SpeedControllerGroup gogo = new SpeedControllerGroup(one,two);
+    private SpeedControllerGroup gogo2 = new SpeedControllerGroup(three,four);
+    private XboxController xbox;
+    private Joystick stick;
     int time = 750;
 
     @Override
     public void robotInit()
     {
         m_myRobot = new DifferentialDrive(gogo,gogo2);
-        xbax = new XboxController(0);
+        xbox = new XboxController(0);
+        stick = new Joystick(1);
     }
 
     @Override
     public void teleopPeriodic()
     {
-        m_myRobot.arcadeDrive((0-1)*xbax.getX(GenericHID.Hand.kLeft),(0-1)*(xbax.getTriggerAxis(GenericHID.Hand.kRight)-(xbax.getTriggerAxis(GenericHID.Hand.kLeft))));
+        m_myRobot.arcadeDrive((0-1)*xbox.getX(GenericHID.Hand.kLeft),(0-1)*(xbox.getTriggerAxis(GenericHID.Hand.kRight)-(xbox.getTriggerAxis(GenericHID.Hand.kLeft))));
+        if(countDown.get() == 0 && stick.getY() < 0)
+        {
+            height.set(stick.getY());
+            countUp.reset();
+        }
+        if(countUp.get() == 0 && stick.getY() > 0)
+        {
+            height.set(stick.getY());
+            countDown.reset();
+        }
+        if(countIn.get() == 0 && stick.getRawButton(1))
+        {
+            grip.set(.3);
+            countOut.reset();
+        }
+        if(countOut.get() == 0 && stick.getRawButton(2))
+        {
+            grip.set(-.3);
+            countIn.reset();
+        }
     }
 
     @Override
@@ -52,20 +83,44 @@ public class Robot extends IterativeRobot {
         gameData = DriverStation.getInstance().getGameSpecificMessage();
         if(gameData.length() > 0)
         {
-            if(gameData.charAt(0) == 'L')
+            //RR option A
+            if(gameData.charAt(0) == 'R' && gameData.charAt(1) == 'R')
             {
                 if(time > 700)
                 {
-                    m_myRobot.arcadeDrive(.005, -90);
+                    m_myRobot.arcadeDrive(.5, -90);
                     time--;
+                    //straight to switch, place and rush scale
                 }
-                else
+            }
+            //RL option A
+            if(gameData.charAt(0) == 'R' && gameData.charAt(1) != 'R')
+            {
+                if(time > 700)
                 {
-                    if(time > 700)
-                    {
-                        m_myRobot.arcadeDrive(.005, -90);
-                        time--;
-                    }
+                    m_myRobot.arcadeDrive(.5, -90);
+                    time--;
+                    //straight to switch, place and rush scale
+                }
+            }
+            //LR option A
+            if(gameData.charAt(0) == 'L' && gameData.charAt(1) != 'L')
+            {
+                if(time>700)
+                {
+                    m_myRobot.arcadeDrive(.5, -90);
+                    time--;
+                    //straight to right side of scale and place
+                }
+            }
+            //LL option A
+            if(gameData.charAt(0) == 'L' && gameData.charAt(1) == 'L')
+            {
+                if(time>700)
+                {
+                    m_myRobot.arcadeDrive(.5, -90);
+                    time--;
+                    //place on left side of switch, then find box, then rush scale
                 }
             }
         }
